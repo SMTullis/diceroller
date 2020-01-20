65880ee37b78910441ea3d3bb062b1bc10411342
Tullis, Sean <seantullis0@gmail.com>
2020-01-20 16:00:13 -0700

    chore: Move general dice rolling and D&D 5e specifics into new files.
    This reorganization aims at generalizing and improving the existing code for superior implementation.

---
+++ dnd5e.py
@@ -0,0 +1,19 @@
+def roll_with_advantage(roll_func):
+    """
+    advantage() rolls two dice and implements a conditional test to return
+    the higher value.
+
+    Arguments:
+    roll_func: a function returning an integer
+    """
+    return max(roll_func(), roll_func())
+
+def roll_with_disadvantage(roll_func):
+    """
+    disadvantage() rolls two dice and implements a conditional test to return
+    the lower value.
+
+    Arguments:
+    roll_func: a function returning an integer
+    """
+    return min(roll_func(), roll_func())
